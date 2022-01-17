af84e1bb711c85e8f1dcb25d8c37ad4a66f8c4b2
Loic <konanloic10@gmail.com>
2022-01-16 22:12:14 -0600

    README.md

--- Assignments/A01/new.cpp
+++ Assignments/A01/new.cpp
@@ -4,10 +4,68 @@
 using namespace std;
 
 void openFile(ifstream &in, ofstream &out);
+void printWorld(string world[], ofstream &out);
+bool valid(int row, int col);
+
 
 const int ROWS = 20;
 const int COLS = 20;
 int visited[ROWS][COLS];    // 2-D array to store which cell is visited
+
+
+int main()
+{    
+    ifstream in;
+    ofstream out;
+
+    openFile(in, out);
+    string world[20]; // World array
+
+    memset(visited, 0, sizeof(visited));    // Initializing visited with 0s
+
+    string line;
+    int i = 0;
+
+    while (getline(in, line))
+    {
+        world[i++] = line;    // Filling the world
+    }
+
+
+    out << "Original world:\n";
+    printWorld(world,out);    // Printing world before mutation
+
+    int count = 0;  // Count of blobs
+    processBlob(world, 0, 0, count);  // Processing blobs
+
+    out << "\n\nWorld with mutated blobs:\n";
+    printWorld(world,out);    // Printing world after mutation
+
+    out << count << " blobs were found.";
+    return 0;
+}
+
+
+void openFile(ifstream &in, ofstream &out)
+{
+    string inName;
+    string outName;
+
+    cout << "Enter the input file name: ";
+    cin >> inName;
+    in.open(inName);
+
+    // Print an error message if file fails to open.
+    if (in.fail())
+    {
+        cout << "Could not open file. \n";
+        cout << "Check file name and location. \n\n";
+    }
+    
+    cout << "Enter the output file name:  ";
+    cin >> outName;
+    out.open(outName);
+}
 
 
 bool valid(int row, int col)
@@ -46,58 +104,4 @@ void printWorld(string world[], ofstream &out)
         }
         out << "\n";
     }
-}
-
-int main()
-{    
-    ifstream in;
-    ofstream out;
-
-    openFile(in,out);
-    string world[20]; // World array
-
-    memset(visited, 0, sizeof(visited));    // Initializing visited with 0s
-
-    string line;
-    int i = 0;
-
-    while (getline(in, line))
-    {
-        world[i++] = line;    // Filling the world
-    }
-
-
-    cout << "Original world:\n";
-    printWorld(world,out);    // Printing world before mutation
-
-    int count = 0;  // Count of blobs
-    processBlob(world, 0, 0, count);  // Processing blobs
-
-    cout << "World with mutated blobs:\n";
-    printWorld(world,out);    // Printing world after mutation
-
-    cout << count << " blobs were found.";
-    return 0;
-}
-
-
-void openFile(ifstream &in, ofstream &out)
-{
-    string inName;
-    string outName;
-
-    cout << "Enter the input file name: ";
-    cin >> inName;
-    in.open(inName);
-
-    // Print an error message if file fails to open.
-    if (in.fail())
-    {
-        cout << "Could not open file. \n";
-        cout << "Check file name and location. \n\n";
-    }
-    
-    cout << "Enter the output file name:  ";
-    cin >> outName;
-    out.open(outName);
 }
