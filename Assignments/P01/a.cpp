06c09f5387ad523f8708a4e0ed0cbd5600732726
Loic <konanloic10@gmail.com>
2022-01-25 02:14:05 -0600

    README.md

---
+++ Assignments/P01/a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+
+
+
+int recursive(int numb)
+{
+    if(numb <= 0)
+        return 1;
+
+    else 
+        return numb + (recursive(numb/2));
+
+}
+int main()
+{
+    int numb;
+    cout << "Enter a number: ";
+    cin >> numb;
+
+    cout << recursive(numb);
+
+    return 0;
+}
