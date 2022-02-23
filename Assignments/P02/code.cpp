5482d05447d92f6f649aeb51bdb290e3e8c89456
Loic <konanloic10@gmail.com>
2022-02-23 00:25:00 -0600

    README.md

--- Assignments/P02/code.cpp
+++ Assignments/P02/code.cpp
@@ -2,3 +2,17 @@
 // You may choose the sorting algorithms, but select one sort from each 
 // runtime category: O(n2), O(n log n), O(n)
 
+// Implement a separate counter for each sort. 
+// Initialize the counter to zero and increment inside loops of the associated sort. 
+// For example, in the O(n2) loop, increment the counter inside the inner loop. 
+// Use a system clock timer to start the time immediately before each sort and end immediately after each sort. 
+// Once you have the code working for one run, average the time and counter results over 20 runs.
+
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+const int NUMBER = 5000;
+
