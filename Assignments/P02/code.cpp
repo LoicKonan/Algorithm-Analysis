9474e172a639cd4c9b1d45d217160d5e8e3eac4f
Loic <konanloic10@gmail.com>
2022-02-23 00:18:48 -0600

    README.md

---
+++ Assignments/P02/code.cpp
@@ -0,0 +1,4 @@
+// Implement 3 different sorting algorithms on arrays of size 5000. 
+// You may choose the sorting algorithms, but select one sort from each 
+// runtime category: O(n2), O(n log n), O(n)
+
