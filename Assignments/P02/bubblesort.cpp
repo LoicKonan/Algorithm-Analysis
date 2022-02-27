cd11b3e27026cf8d886d58ff7e9c330e8c533b55
Loic <konanloic10@gmail.com>
2022-02-27 00:30:57 -0600

    README.md

--- Assignments/P02/bubblesort.cpp
+++ Assignments/P02/bubblesort.cpp
@@ -11,12 +11,14 @@ void fillArray(int arr[]);
 void copyArray(int arr1[], int arr2[]);
 void printArray(int arr[]);
 void swap(int &x, int &y);
+
 int bubbleSort(int arr[]);
 
 int partition(int a[], int beg, int end);
 void quickSort(int a[], int p, int r);
 
-int CountingsSort(int arr[]);
+void countSort(int array[], int);
+
 
 int main() 
 {
@@ -49,13 +51,15 @@ int main()
   cout << "Array 2 sorted with Quick Sort: \n";
   printArray(myArray2);
 
+  int n = sizeof(myArray3) / sizeof(myArray3[0]);
 
   cout << "Array 3:  \n";
   printArray(myArray3);
-  CountingsSort(myArray3);
+  countSort(myArray3, n);
   cout << "radix count:  " << CountingCount << endl;
   cout << "Array 3 sorted with Radix Sort: \n"; 
   printArray(myArray3);
+
   return 0;
 }
 
@@ -164,4 +168,54 @@ void quickSort(int a[], int p, int r)
     // Recursively sort the array to the right of the pivot      
 		quickSort(a, q + 1, r);			    
 	}
+}
+
+void countSort(int array[], int SIZE) 
+{
+  // The SIZE of count must be at least the (max+1) but
+  // we cannot assign declare it as int count(max+1) in C++ as
+  // it does not support dynamic memory allocation.
+  // So, its SIZE is provided statically.
+  int output[10];
+  int count[10];
+  int max = array[0];
+
+  // Find the largest element of the array
+  for (int i = 1; i < SIZE; i++) 
+  {
+    if (array[i] > max)
+      max = array[i];
+  }
+
+  // Initialize count array with all zeros.
+  for (int i = 0; i <= max; ++i) 
+  {
+    count[i] = 0;
+  }
+
+  // Store the count of each element
+  for (int i = 0; i < SIZE; i++) 
+  {
+    count[array[i]]++;
+  }
+
+  // Store the cummulative count of each array
+  for (int i = 1; i <= max; i++) 
+  {
+    count[i] += count[i - 1];
+  }
+
+  // Find the index of each element of the original array in count array, and
+  // place the elements in output array
+  for (int i = SIZE - 1; i >= 0; i--) 
+  {
+    output[count[array[i]] - 1] = array[i];
+    count[array[i]]--;
+  }
+
+  // Copy the sorted elements into original array
+  for (int i = 0; i < SIZE; i++) 
+  {
+    array[i] = output[i];
+  }
 }
