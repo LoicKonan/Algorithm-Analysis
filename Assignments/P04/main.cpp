11091fd20863a711b933a9d74cc753a228d3dd79
Loic <konanloic10@gmail.com>
2022-04-08 00:59:43 -0500

    README.md

---
+++ Assignments/P04/main.cpp
@@ -0,0 +1,18 @@
+// Program to store a list of names and phone numbers using a doubly linked list structure.
+// Read in a list of ten names and phone numbers from a names.txt 
+// Store the information alphabetically by last name, then by first name. 
+// Allow the user to select from a menu of the following choices: 
+// Add to the list, delete from the list, look up phone number given name,
+// look up name given phone number, display list, and exit. 
+
+
+#include <iostream>
+#include <random>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    
+}
